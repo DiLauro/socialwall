d843a10482c3b3e11a9e2d074ff9370325500855
P Smith <schmidtpaolo@gmail.com>
2018-03-11 12:59:16 +0100

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,7 +1,7 @@
 
 var FB = require('fb');
 
-FB.setAccessToken('EAAZA0SWFabd8BADMlSZCZAqZAljKBDZAOSgNpNPcmizle9QBwCyQ4rceZAakggGUM9DZAGnN5LQpf0vpIXqMFFbKJoDlZB8Qkomykv0Paz7yatwcrEQ86FDMA9ZCwRfAltm4bUSxi3KQt6PhxYNimK1g0iUzjdDxBJ6YBDR4pNqZATDCtDjVcn38i3jpeV0uxN1bAZD');
+FB.setAccessToken('');
 
  FB.api('#trump', function (res) {
   if(!res || res.error) {
