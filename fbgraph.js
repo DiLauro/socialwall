c10525c0f7dc4f2ecb1a41df368681ba6f5f0264
P Smith <schmidtpaolo@gmail.com>
2018-03-11 12:58:23 +0100

    Update fbgraph.js

--- fbgraph.js
+++ fbgraph.js
@@ -1,6 +1,6 @@
 var graph = require('fbgraph');
 
-graph.setAccessToken('EAAZA0SWFabd8BADMlSZCZAqZAljKBDZAOSgNpNPcmizle9QBwCyQ4rceZAakggGUM9DZAGnN5LQpf0vpIXqMFFbKJoDlZB8Qkomykv0Paz7yatwcrEQ86FDMA9ZCwRfAltm4bUSxi3KQt6PhxYNimK1g0iUzjdDxBJ6YBDR4pNqZATDCtDjVcn38i3jpeV0uxN1bAZD');
+graph.setAccessToken('');
 
 
 graph.setVersion("2.8");
